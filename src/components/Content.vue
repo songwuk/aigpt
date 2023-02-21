431617bb8b00ba0b022633a71d6469343e6388d6
songwuk <1017301325@qq.com>
2023-02-21 21:23:49 +0800

    feat: upgrade

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import Left from './img/left.png'
+import LeftOff from './img/leftoff.png'
 import FenLeiOff from './img/fenleioff.png'
 import FenLei from './img/fenlei.png'
 import Filter from './img/filter.png'
@@ -10,6 +11,13 @@ import ShoucangOff from './img/shoucangoff.png'
 import JurassicWait from './img/jurassicwait.png'
 import JurassicWaitOff from './img/jurassicwaitoff.png'
 import Bottom from './img/bottom.png'
+import Right from './img/right.png'
+import Search from './img/search.png'
+import Quest from './img/quest.png'
+import See from './img/see.png'
+import Like from './img/like.png'
+import Start from './img/start.png'
+import StartOff from './img/startOff.png'
 const stable = ref([
   {
     name: 'Categories',
@@ -32,18 +40,102 @@ const stable = ref([
     off: JurassicWaitOff,
   },
 ])
+const leftStatus = ref([
+  {
+    name: 'Model',
+    children: [
+      {
+        name: 'ChatGPT',
+        num: 1,
+      },
+      {
+        name: 'OpenAI CLIP',
+        num: 22,
+      },
+      {
+        name: 'DALL·E',
+        num: 1,
+      },
+    ],
+    bar: false,
+  },
+  {
+    name: 'Generative AI',
+    children: [
+      {
+        name: 'Text to Text',
+        num: 1,
+      },
+      {
+        name: 'Text to Image',
+        num: 22,
+      },
+      {
+        name: 'Text to Video',
+        num: 22,
+      },
+      {
+        name: 'Text to Code',
+        num: 22,
+      },
+      {
+        name: 'Text to Audio',
+        num: 22,
+      },
+      {
+        name: 'Text to 3D',
+        num: 22,
+      },
+    ],
+    bar: false,
+  },
+  {
+    name: 'Application',
+    children: [
+      {
+        name: 'Website',
+        num: 1,
+      },
+      {
+        name: 'Apps',
+        num: 22,
+      },
+      {
+        name: 'Web3',
+        num: 22,
+      },
+      {
+        name: 'Chrome Extension',
+        num: 1,
+      },
+    ],
+    bar: false,
+  },
+])
 const categories = ref('')
 const changeOnce = () => {
 
 }
+const leftshow = ref(true)
+const leftOff = () => {
+  leftshow.value = !leftshow.value
+}
+const clickBar = (idx) => {
+  leftStatus.value[idx].bar = !leftStatus.value[idx].bar
+}
+const clickBarStatus = ref('')
+const searchAll = (name) => {
+  clickBarStatus.value = name
+}
+const startStatus = ref(false)
 </script>
 
 <template>
-  <div class="bg-[#201f24] pl-[158px] pr-[161px]" w-full>
+  <div class="bg-[#201f24] 2xl:pl-[158px] 2xl:pr-[161px] pl-[2vw] pr-[2vw]" w-full>
     <div class="leftBottom pt-[27px] pb-[19px]" flex items-center justify-start>
-      <div flex items-center justify-center>
+      <div flex items-center justify-center @click="leftOff">
         <span cursor-pointer flex items-center justify-center block class=" bg-[#475369] rounded-[23px] 2xl:w-[72px] 2xl:h-[46px] w-12 h-8">
-          <img class="w-[20px] h-[20px]" :src="Left" alt="Left">
+          <img class="w-[20px] h-[20px]" :src="leftshow ? LeftOff : Left" alt="Left">
         </span>
       </div>
       <div flex items-center justify-center class="2xl:ml-36px ml-1">
@@ -69,23 +161,79 @@ const changeOnce = () => {
         </span>
       </div>
       <aside w-full>
-        <div class="px-4 py-1 pr-8" flex items-center justify-center style="height: 46px;background: #3C3C3E;border-radius: 26px;">
+        <div class="px-4 py-1 pr-8" baseline flex items-center justify-center style="height: 46px;background: #3C3C3E;border-radius: 26px;">
           <input v-model="categories" class="c-[#6D6D6D]" placeholder="Search for apps,categories" style="background: #3C3C3E;" outline-none border-none w-full type="text">
           <img
             cursor-pointer
-            class="label_5"
-            referrerpolicy="no-referrer"
-            src="https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng4f735fd0eb05095211e2b6cfeb3c978aaf0bfc7fb16535ee915ed2780cc68f9e"
+            class="w-[22px]"
+            :src="Search"
           >
         </div>
       </aside>
     </div>
-    <span mt-2>++++</span>
+    <div
+      flex items-start justify-start style="font-family: Helvetica;"
+    >
+      <aside v-show="leftshow" class="pr-[19px] w-50 2xl:text-lg 2xl:w-[236px] border-r b-r-[#6D6D6D]" c-white>
+        <div v-for="(item, index) in leftStatus" :key="index" cursor-pointer class="p-b-[15px] pt-[19px] leftBar " @click.stop="clickBar(index)">
+          <div flex items-center justify-between>
+            <span>{{ item.name }}</span>
+            <i class="pr-[1px]"><img class="w-[10px]" :src="Right" alt="Right"></i>
+          </div>
+          <aside v-if="item.bar" class="2xl:ml-13px 2xl:text-sm c-[#D8D8D8] ml-5">
+            <div v-for="(it, idx) in item.children" :key="idx" flex items-center justify-between class=" my-2 2xl:my-13px hover:c-[#05D4FD]" :class="[clickBarStatus === it.name ? 'c-[#05D4FD]' : '']" @click.stop="searchAll(it.name)">
+              <span> {{ it.name }}</span>
+              <span> {{ it.num }}</span>
+            </div>
+          </aside>
+        </div>
+      </aside>
+      <section class="ml-[22px] mt-[25px]" w-full>
+        <span c-white>{{ clickBarStatus }}</span>
+        <div class="mt-24px">
+          <div relative c-white class="w-[320px] h-118px rounded-[10px]" style="border: 1px solid rgb(151 151 151 / 33%);">
+            <div class="w-[320px] h-118px rounded-[10px]" />
+            <div absolute top-0 left-0 right-0 z-2>
+              <div flex items-center justify-end class="mt-[12px] mr-[18px] text-sm">
+                <span flex items-center justify-center><img
+                  :src="See"
+                  class="w-16px mr-4px"
+                >22</span>
+                <span cursor-pointer flex items-center justify-center><img
+                  :src="Like"
+                  class="w-14px mr-4px ml-8px"
+                >21</span>
+                <img
+                  cursor-pointer
+                  :src="startStatus ? Start : StartOff"
+                  class="w-16px ml-8px"
+                  @click="startStatus = !startStatus"
+                >
+              </div>
+              <div class="ml-18px" flex items-center justify-start>
+                <img
+                  class="w-[32px]"
+                  :src="Quest"
+                  alt="Quest"
+                >
+                <span class="ml-11px">Quest 3</span>
+              </div>
+              <div class="ml-18px mt-12px">
+                Arrakis is a protocol specialized in
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .leftBottom {
+  border-bottom: 1px solid #6D6D6D;
+}
+.leftBar:not(:last-child) {
   border-bottom: 1px solid #6D6D6D;
 }
 </style>
