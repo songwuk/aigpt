9602aa8c4401b4dfe2516e45062ec4a76d1bffef
songwuk <1017301325@qq.com>
2023-02-27 15:51:47 +0800

    chore: 更新聊天的接口

--- src/url.ts
+++ src/url.ts
@@ -1,8 +1,8 @@
 import { createFetch } from '@vueuse/core'
-import { ref } from 'vue'
+import { ref, unref } from 'vue'
 const url = ref('https://test-api.aigpt.me')
 const useFetchOptions = createFetch({
-  baseUrl: url.value,
+  baseUrl: unref(url),
   options: {
     async afterFetch<T>({ response, data }: { response: Response; data: T | null }) {
       return {
@@ -54,5 +54,57 @@ export function productsSave(params: any, files: any) {
     // headers: {
     //   'Content-Type': 'application/x-www-form-urlencoded',
     // },
+  })
+}
+/**
+ * 根据分享的id获取分享内容
+ * @param id
+ * @returns
+ */
+export function getShare(id: string) {
+  return useFetchOptions(`/share/${id}`, {
+    method: 'GET',
+  })
+}
+/**
+ * 提交分享
+ * @param {Array<{q:'',a:''}>} params
+ * @returns
+ */
+export function pushShare(params) {
+  return useFetchOptions('/share/save', {
+    method: 'POST',
+    body: JSON.parse(params),
+  })
+}
+/**
+ * 根据分组来获取所以的记录
+ * @param { Object } params
+ * @returns
+ */
+export function getPageOfChat(params) {
+  return useFetchOptions('/openai/pageOfChat', {
+    method: 'POST',
+    body: JSON.parse(params),
+  })
+}
+/**
+ * 获取所以的分组列表
+ * @returns
+ */
+export function loadChatGroup() {
+  return useFetchOptions('/openai/loadChatGroup', {
+    method: 'GET',
+  })
+}
+/**
+ * 聊天
+ * @param params
+ * @returns
+ */
+export function openaiComletions(params) {
+  return useFetchOptions('/openai/comletions', {
+    method: 'GET',
+    body: JSON.stringify(params),
   })
 }
