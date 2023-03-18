6c527af18f90e316127a90e49a902ab1263bd140
songwuk <1017301325@qq.com>
2023-03-18 22:11:08 +0800

    refactor: chat

--- src/components/NewChat.vue
+++ src/components/NewChat.vue
@@ -38,8 +38,7 @@ const leftShow = ref(false)
 const clickshow = () => {
   leftShow.value = !leftShow.value
 }
-
-const themes = ref<Array<string>>(['light', 'dark'])
+const leftBarStatus = ref(null)
 const theme = ref<string | null | undefined>(null)
 const setTheme = async (themeStatus?: string) => {
   const themsStatus = async () => {
@@ -259,7 +258,7 @@ const clickMethods = async (id) => {
     }
   }
 }
-const pageChat = async (chatGroup) => {
+const pageChat = async (chatGroup, index) => {
   const params = {
     condition: chatGroup,
   }
@@ -267,6 +266,7 @@ const pageChat = async (chatGroup) => {
   const dataSource = data.value as any
   if (dataSource && dataSource.code === 0) {
     console.log(dataSource)
+    leftBarStatus.value = index
     list.value = dataSource.data.openaiList ?? []
     showAnswer.value = false
     console.log(list.value)
@@ -492,11 +492,13 @@ onMounted(async () => {
             </a>
             <div class="flex-col flex-1 overflow-y-auto border-b border-white/20 -mr-2">
               <div class="flex flex-col gap-2 text-gray-100 text-sm">
-                <a v-for="(item, index) in historyData" :key="index" class="flex py-3 px-3 items-center gap-3 relative rounded-md hover:bg-[#2A2B32] cursor-pointer break-all hover:pr-4 group" @click="pageChat(item)">
+                <a v-for="(item, index) in historyData" :key="index" :class="[leftBarStatus === index ? 'bg-gray-700' : '']" class="flex py-3 px-3 items-center gap-3 relative rounded-md hover:bg-gray-700 cursor-pointer break-all" @click="pageChat(item, index);">
                   <img class="w-17px" :src="NewChat" alt="NewChat">
                   <div class="flex-1 text-ellipsis max-h-5 overflow-hidden break-all relative">
                     {{ item }}
-                    <div class="absolute inset-y-0 right-0 w-8 z-10 bg-gradient-to-l from-gray-900 group-hover:from-[#2A2B32]" />
+                    <div v-if="leftBarStatus === index" class="absolute inset-y-0 right-0 z-10 ">
+                      <div i-carbon-trash-can />
+                    </div>
                   </div>
                 </a>
               </div>
@@ -551,7 +553,7 @@ onMounted(async () => {
                     <a class="flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm mb-2 flex-shrink-0 border border-white/20"><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><line x1="12" y1="5" x2="12" y2="19" /><line x1="5" y1="12" x2="19" y2="12" /></svg>New chat</a>
                     <div class="flex-col flex-1 overflow-y-auto border-b border-white/20">
                       <div class="flex flex-col gap-2 text-gray-100 text-sm">
-                        <a v-for="(item, index) in historyData" :key="index" class="flex py-3 px-3 items-center gap-3 relative rounded-md cursor-pointer break-all pr-14 bg-black-800 hover:bg-gray-800 group" @click="pageChat(item)">
+                        <a v-for="(item, index) in historyData" :key="index" class="flex py-3 px-3 items-center gap-3 relative rounded-md cursor-pointer break-all pr-14 bg-black-800 hover:bg-gray-800 group" @click="pageChat(item, index)">
                           <img class="w-17px" :src="NewChat" alt="NewChat">
                           <div class="flex-1 text-ellipsis max-h-5 overflow-hidden break-all relative">{{ item }}</div>
                         </a>
