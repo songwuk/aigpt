050f4c39ee3829ba9493501c05275a44dd91c40a
songwuk <1017301325@qq.com>
2023-03-19 18:27:24 +0800

    chore: admin 优化

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -27,6 +27,24 @@ const listProducts = ref([{
   action: 'button',
   trending: false,
 }])
+
+const listRegisters = ref([{
+  id: 1,
+  email: 'yeet@gmail.com',
+  address: '0x58A9130D18Ec2c41D535d56B7E2a616BBeC6f359',
+  date: '02/03/2023',
+  credits: '5,000',
+  upgradeplan: '',
+  creditgifts: '',
+}, {
+  id: 2,
+  email: 'yeet@gmail.com',
+  address: '0x58A9130D18Ec2c41D535d56B7E2a616BBeC6f359',
+  date: '02/03/2023',
+  credits: '5,000',
+  upgradeplan: '',
+  creditgifts: '',
+}])
 const page = ref([
 
 ])
@@ -50,7 +68,7 @@ const page = ref([
         </div>
       </div>
     </aside>
-    <div relative overflow-x-hidden overflow-y-scroll class="w-[calc(100%-265px)] h-[calc(100vh-114px)] pt-40px pl-51px pr-118px bg-#26262C ">
+    <div v-if="isProducts" relative overflow-x-hidden overflow-y-scroll class="w-[calc(100%-265px)] h-[calc(100vh-114px)] pt-40px pl-51px pr-118px bg-#26262C ">
       <h3 class="text-30px">
         Products <span ml-30px>685</span>
       </h3>
@@ -70,29 +88,61 @@ const page = ref([
           <span text-center inline-block class="w-145px h-80px leading-80px border-transparent border-2">Action</span>
         </div>
         <div class="content">
-          <div v-for="(item, index) in listProducts" :key="index" class="-mt-10px" :class="[index % 2 !== 0 ? 'bg-#333944' : '']">
-            <span text-center inline-block class="w-145px h-80px  leading-80px border-transparent border-2 b-r-#A0A0A0">{{ item.id }}</span>
-            <span text-center inline-block class="w-140px h-80px leading-80px  border-transparent border-2 b-r-#A0A0A0">{{ item.name }}</span>
-            <span text-center inline-block class="w-144px h-80px leading-80px border-transparent border-2 b-r-#A0A0A0">{{ item.email }}</span>
-            <span text-center inline-block class="w-150px h-80px leading-80px border-transparent border-2 b-r-#A0A0A0">{{ item.website }}</span>
-            <span text-center inline-block class="w-146px h-80px leading-80px border-transparent border-2 b-r-#A0A0A0">{{ item.date }}</span>
-            <span text-center inline-block class="w-140px h-80px leading-80px border-transparent border-2 b-r-#A0A0A0">{{ item.views }}</span>
-            <span text-center inline-block class="w-145px h-80px leading-80px border-transparent border-2 b-r-#A0A0A0">{{ item.likes }}</span>
-            <span text-center inline-block class="w-150px h-80px leading-80px  border-transparent border-2 b-r-#A0A0A0 c-#F7B500">{{ item.status }}</span>
-            <span text-center inline-block class="w-155px  h-80px leading-80px border-transparent border-2 b-r-#A0A0A0">
+          <div v-for="(item, index) in listProducts" :key="index" :class="[index % 2 !== 0 ? 'bg-#333944' : '']">
+            <span text-center inline-block class="lineText w-145px h-80px leading-80px border-transparent ">{{ item.id }}</span>
+            <span text-center inline-block class="lineText w-140px h-80px leading-80px  border-transparent">{{ item.name }}</span>
+            <span text-center inline-block class="lineText w-144px h-80px leading-80px border-transparent">{{ item.email }}</span>
+            <span text-center inline-block class="lineText w-150px h-80px leading-80px border-transparent">{{ item.website }}</span>
+            <span text-center inline-block class="lineText w-146px h-80px leading-80px border-transparent">{{ item.date }}</span>
+            <span text-center inline-block class="lineText w-140px h-80px leading-80px border-transparent ">{{ item.views }}</span>
+            <span text-center inline-block class="lineText w-145px h-80px leading-80px border-transparent">{{ item.likes }}</span>
+            <span text-center inline-block class="lineText w-150px h-80px leading-80px  border-transparent c-#F7B500">{{ item.status }}</span>
+            <span text-center inline-block class="lineText w-155px  h-80px leading-80px border-transparent">
               <span inline-block relative class="w-93px h-31px  border rounded-8px border-transparent pt-10px" cursor-pointer @click="item.trending = !item.trending">
                 <i absolute z-1 inline-block class="w-50px h-31px leading-31px border rounded-8px border-transparent -left-5px " :class="[item.trending ? 'bg-#004699 z-2' : 'bg-#B5BFCB']">On</i>
                 <i absolute z-1 inline-block class=" -ml10px w-50px h-31px leading-31px border rounded-8px border-transparent" :class="[!item.trending ? 'bg-#004699  z-2' : 'bg-#B5BFCB']">Off</i>
               </span>
             </span>
-            <span text-center inline-block class="w-145px border-transparent border-2">
+            <span text-center inline-block class=" w-145px">
               <span cursor-pointer inline-block class="w-93px h-40px leading-40px bg-#004699 border rounded-8px border-transparent">Review</span>
             </span>
           </div>
         </div>
       </aside>
       <div class="page b-#26262C absolute right-0 pr-118px mt-20px">
-        <el-pagination background layout="prev, pager, next" :total="100" />
+        <el-pagination background layout="prev, pager, next" :page-size="10" :total="100" />
+      </div>
+    </div>
+    <div v-else relative overflow-x-hidden overflow-y-scroll class="w-[calc(100%-265px)] h-[calc(100vh-114px)] pt-40px pl-51px pr-118px bg-#26262C ">
+      <h3 class="text-30px">
+        Registers <span ml-30px> 32,673</span>
+      </h3>
+      <aside
+        class="mt-16px w-1481px h-auto border-4px border-solid border-#FFFFFF"
+      >
+        <div class="header text-20px" :class="[bgColor]">
+          <span text-center inline-block class="w-148px h-80px leading-80px border-transparent border-2 b-r-#A0A0A0">Number</span>
+          <span text-center inline-block class="w-155px h-80px leading-80px border-transparent border-2 b-r-#A0A0A0">Email</span>
+          <span text-center inline-block class="w-290px h-80px leading-80px border-transparent border-2 b-r-#A0A0A0">Address</span>
+          <span text-center inline-block class="w-150px h-80px leading-80px border-transparent border-2 b-r-#A0A0A0">Date</span>
+          <span text-center inline-block class="w-150px h-80px leading-80px border-transparent border-2 b-r-#A0A0A0">Credits</span>
+          <span text-center inline-block class="w-290px h-80px leading-80px border-transparent border-2 b-r-#A0A0A0">Upgrade Plan</span>
+          <span text-center inline-block class="w-290px h-80px leading-80px border-transparent ">Credit Gifts</span>
+        </div>
+        <div class="content">
+          <div v-for="(item, index) in listRegisters" :key="index" flex :class="[index % 2 !== 0 ? 'bg-#333944' : '']">
+            <span text-center inline-block class="lineText w-148px h-80px  leading-80px ">{{ item.id }}</span>
+            <span text-center inline-block class="lineText w-155px h-80px leading-80px ">{{ item.email }}</span>
+            <span text-center inline-block class="lineText w-290px h-80px leading-80px">{{ item.address }}</span>
+            <span text-center inline-block class="lineText w-150px h-80px leading-80px">{{ item.date }}</span>
+            <span text-center inline-block class="lineText w-150px h-80px leading-80px">{{ item.credits }}</span>
+            <span text-center inline-block class="lineText w-290px h-80px leading-80px">{{ item.upgradeplan }}</span>
+            <span text-center inline-block class=" w-290px h-80px leading-80px border-transparent">{{ item.creditgifts }}</span>
+          </div>
+        </div>
+      </aside>
+      <div class="page b-#26262C absolute right-0 pr-118px mt-20px">
+        <el-pagination background layout="prev, pager, next" :page-size="10" :total="32673" />
       </div>
     </div>
   </section>
@@ -112,5 +162,20 @@ const page = ref([
   background-color: #26262C !important;
   border: 1px solid #FFFFFF;
   color: white
+}
+.lineText {
+  position: relative;
+}
+.lineText::after{
+  position: absolute;
+  right:0;
+  top: -5px;
+  bottom:0;
+  display: inline-block;
+  content: '';
+  width: 2px;
+  height: 85px;
+  background-color: #A0A0A0;
+
 }
 </style>
