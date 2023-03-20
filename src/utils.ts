ac74b6d606852f037ea7131cf076572f94e6f684
songwuk <1017301325@qq.com>
2023-03-20 21:42:01 +0800

    feat: 工具类

--- src/utils.ts
+++ src/utils.ts
@@ -22,3 +22,18 @@
 // }).catch((error) => {
 //   // validation failed and userinfo was not obtained
 // })
+
+export class NumUtils {
+  static formatWithK(num) {
+    const absNum = Math.abs(num) // 取绝对值
+    if (absNum >= 1000000) { // 大于等于100万
+      return `${(num / 1000000).toFixed(1)}M`
+    }
+    else if (absNum >= 1000) { // 大于等于1千
+      return `${(num / 1000).toFixed(1)}k`
+    }
+    else {
+      return num.toString()
+    }
+  }
+}
