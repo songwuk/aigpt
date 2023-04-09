4bf261cb3c09291d64df3dc3d0922b9452e2c887
songwuk <1017301325@qq.com>
2023-04-09 17:46:31 +0800

    fix: add proxy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,16 @@ export default defineConfig({
   // Enable Vue to support Vue components.
   experimental: { integrations: true },
   integrations: [vue({ appEntrypoint: '/src/pages/_app/app' }), UnoCSS()],
+  vite: {
+    server: {
+      proxy: {
+        '/chatai': {
+          // target: 'http://10.33.236.71:8080/gap-h5-backend',
+          target: 'https://test-api.aigpt.me',
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/chatai/, ''),
+        },
+      },
+    },
+  },
 })
